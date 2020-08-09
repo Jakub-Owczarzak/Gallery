cd4018bc7bd21627cb6d4f850edb94cd8faa032d
Jakub Owczarzak <66114694+Jakub-Owczarzak@users.noreply.github.com>
2020-08-09 14:04:59 +0200

    Update lightbox.js

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -44,7 +44,7 @@
     albumLabel: 'Zdjęcie %1 of %2',
     alwaysShowNavOnTouchDevices: true,
     fadeDuration: 600,
-    fitImagesInViewport: true,
+    fitImagesInViewport: false,
     imageFadeDuration: 600,
     // maxWidth: 800,
     // maxHeight: 600,
